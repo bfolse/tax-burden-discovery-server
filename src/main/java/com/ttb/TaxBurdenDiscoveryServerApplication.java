2fa264675d986a28b6e53f23d66126235427ebe2
Brian Folse <bmfolse@gmail.com>
2018-10-17 15:03:32 -0400

    AWS setup

--- src/main/java/com/ttb/TaxBurdenDiscoveryServerApplication.java
+++ src/main/java/com/ttb/TaxBurdenDiscoveryServerApplication.java
@@ -1,8 +1,13 @@
 package com.ttb;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,5 +15,14 @@ public class TaxBurdenDiscoveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaxBurdenDiscoveryServerApplication.class, args);
+	}
+
+	@Bean
+	@Profile("awsdev")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		b.setDataCenterInfo(info);
+		return b;
 	}
 }
